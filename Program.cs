bf876695a8602732a29215d30840cea3f42bb0b0
jaymecsn <jayme.conselheiro@gmail.com>
2023-11-30 18:54:13 -0300

    Agora o programa roda até o usuário escolher sair.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿void intro()
-{
-    System.Console.WriteLine("Seja bem vindo(a) ao");
+{   
+
+    Console.Clear();
+
+    System.Console.WriteLine("\nSeja bem vindo(a) ao");
     System.Console.WriteLine(@"
  
 ██████╗░███╗░░██╗░██████╗░  ██████╗░██████╗░░█████╗░░░░░░██╗███████╗░█████╗░████████╗
@@ -13,11 +16,13 @@
 }
 
 void menu()
-{
-    System.Console.WriteLine("Por favor escolha entre as seguintes opções:");
-    System.Console.WriteLine("Digite 1 para - Jogo de adivinhação");
-    System.Console.WriteLine("Digite 2 para - Gerar números aleatórios");
-    System.Console.WriteLine("Digite 3 para SAIR\n\n");
+{   
+    intro();
+
+    System.Console.WriteLine("Por favor escolha entre as seguintes opções:\n");
+    System.Console.WriteLine("Digite 1 para - Jogo de adivinhação;");
+    System.Console.WriteLine("Digite 2 para - Gerar números aleatórios;");
+    System.Console.WriteLine("Digite 3 para SAIR;");
 
     System.Console.Write("\nDigite a sua opção agora: ");
     string impString = System.Console.ReadLine()!;
@@ -34,12 +39,15 @@ void menu()
         case 3:System.Console.WriteLine("\nVocê escolheu a opção: " + imp + "\nMuito obrigado por entrar.\nAté a próxima!");
             break;
         default:System.Console.WriteLine("\nVocê escolheu a opção: " + imp +"\nEsta opção está fora do escopo. Sinto muito.");
+            menu();
             break;
     }
 }
 
 void jogorng()
 {
+    Console.Clear();
+
     System.Console.WriteLine("Com quantos números você quer jogar?");
     int resp = int.Parse(System.Console.ReadLine()!);
     Random numero = new Random();
@@ -48,24 +56,41 @@ void jogorng()
 
     do
     {
-        System.Console.WriteLine("Digite um número de 1 a " + resp);
+        System.Console.WriteLine("\nDigite um número de 1 a " + resp);
         int chute = int.Parse(System.Console.ReadLine()!);
 
         if(chute == Num)
         {
-            System.Console.WriteLine("Isso o número era " + Num + " você venceu o jogo!\nAté a próxima!\nTschuss!");
+            System.Console.WriteLine($"Isso o número era {Num} você venceu o jogo!");
+            System.Console.WriteLine("\nPrecione qualquer tecla para continuar.");
+            Console.ReadKey();
             break;
         }else if(chute < Num){
-            System.Console.WriteLine("O número é maior que " + chute);
+            System.Console.WriteLine($"O número é maior que {chute}\n");
         }else if(chute > Num){
-            System.Console.WriteLine("O número é menor que " + chute);
+            System.Console.WriteLine($"O número é menor que {chute}\n");
         }
 
     } while (true);
+
+    Console.Clear();
+
+    System.Console.WriteLine("Quer jogar novamente?\nDigite 1 para sim e 0 para não!");
+    System.Console.WriteLine("Você está no Jogo da adivinhação.");
+    int playAgain = int.Parse(System.Console.ReadLine()!);
+    if(playAgain == 0)
+    {
+        menu();
+    }else{
+        jogorng();
+    }
+
 }
 
 void randomNums()
 {
+    Console.Clear();
+
     System.Console.WriteLine("\nQuantos números aleatórios vamos gerar?");
     int resp = int.Parse(System.Console.ReadLine()!);
 
@@ -80,7 +105,17 @@ void randomNums()
     {
         System.Console.WriteLine(numero.Next(min,max+1));
     }
-    System.Console.WriteLine("\nObrigado por gerar números conosco.\nAté a próxima!\nTschuss!");
+
+    System.Console.WriteLine("Quer fazer novamente?\nDigite 1 para sim e 0 para não!");
+    System.Console.WriteLine("Você está no Gerador de números.");
+    int playAgain = int.Parse(System.Console.ReadLine()!);
+    if(playAgain == 0)
+    {
+        menu();
+    }else{
+        randomNums();
+    }
+    
 }
 
 intro();
